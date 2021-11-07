4e8af84cb8982f23a20c0a14cfdc19142d0744e2
Jesse Taube <Mr.Bossman075@gmail.com>
2021-11-06 22:25:08 -0400

    gatesOS? formating

--- colorcat.c
+++ colorcat.c
@@ -9,27 +9,44 @@
 #include <errno.h>
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-  #include <windows.h>
+#include <windows.h>
+ssize_t getline(char **ptr, size_t *n, FILE *stream)
+{
+  if (!*ptr)
+    *ptr = malloc(100);
+  if (fgets(*ptr, 100, stream) == NULL)
+    return -1;
+  if (ferror(stream))
+    return -1;
+  *n = strnlen(*ptr, 100);
+  return *n;
+}
 #endif
 
 #if __has_include(<unistd.h>)
-  #include <sys/time.h>
-  #define POSIX
+#include <sys/time.h>
+#define POSIX
 #endif
+
+#ifndef uint
+#define uint uint32_t
+#endif
+
+extern char *optarg;
 
 static bool color_256 = true;
 static bool each_char = true;
 static bool rotate_color = true; // inside chars only works if each char is chosen
 static bool random_start_color = false;
 static uint color_count = 50; // N hues
-static uint start_count = 0; // Starting hue
+static uint start_count = 0;  // Starting hue
 static uint shift_c = 1;
 
 static void clear(int sig);
 static uint hue_to_ansiNum(double hue);
 static void Scolor(uint color);
 static void color(uint color);
-static void pchar(const char* str,uint start);
+static void pchar(const char *str, uint start);
 static void help(int code);
 static double dmod(double x, double y);
 
@@ -37,61 +54,72 @@ static double dmod(double x, double y);
 static void init_rand();
 static int c_rand();
 static long long time_millis();
-static FILE * Fdat;
+static FILE *Fdat;
 
 #ifdef POSIX
-static long long time_millis() {
+static long long time_millis()
+{
   struct timeval te;
   gettimeofday(&te, NULL);
-  long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000;
+  long long milliseconds = te.tv_sec * 1000LL + te.tv_usec / 1000;
   return milliseconds;
 }
-#elif defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-static long long time_millis() {
+#elif defined(WIN32) || defined(_WIN32) || defined(__WIN32)
+static long long time_millis()
+{
   SYSTEMTIME time;
   GetSystemTime(&time);
   LONG time_ms = (time.wSecond * 1000) + time.wMilliseconds;
   return time_ms;
 }
 #else
-static long long time_millis() {
+static long long time_millis()
+{
   return time(NULL);
 }
 #endif
 
-static int c_rand(){
-  if (!Fdat) {
-    srand(time_millis(NULL)+rand());
+static int c_rand()
+{
+  if (!Fdat)
+  {
+    srand(time_millis(NULL) + rand());
     return rand();
-  } else {
+  }
+  else
+  {
     int rand_data;
     fread(&rand_data, 1, sizeof(int), Fdat);
     return rand_data;
   }
 }
-static void init_rand(){
+static void init_rand()
+{
   Fdat = fopen("/dev/urandom", "r");
   srand(time_millis(NULL));
 }
 /* Random end */
 
-double dmod(double x, double y) {
-    return x - (int)(x/y) * y;
+double dmod(double x, double y)
+{
+  return x - (int)(x / y) * y;
 }
 
-static void clear(int sig){
+static void clear(int sig)
+{
   puts("\033[0m");
   fclose(Fdat);
   exit(sig);
 }
 
-static uint hue_to_ansiNum(double hue) {
+static uint hue_to_ansiNum(double hue)
+{
   const double sat = 1.0;
   const double gam = 0.75;
   double p, q, t, ff;
   double r, g, b;
   long i;
-  hue = dmod(hue,360.0);
+  hue = dmod(hue, 360.0);
   hue /= 60.0;
   i = (long)hue;
   ff = hue - i;
@@ -132,44 +160,50 @@ static uint hue_to_ansiNum(double hue) {
     b = q;
     break;
   }
-    r *= 6;
-    g *= 6;
-    b *= 6;
-    return 16 + (36*(int)r) + (6*(int)g) + (int)b;
+  r *= 6;
+  g *= 6;
+  b *= 6;
+  return 16 + (36 * (int)r) + (6 * (int)g) + (int)b;
 }
 
-static void Scolor(uint color) {
-  printf("\033[38;5;%um",color);
+static void Scolor(uint color)
+{
+  printf("\033[38;5;%um", color);
 }
 
-static void color(uint color) {
-  if(color_256)
-    Scolor(hue_to_ansiNum(((color*360)/color_count)+start_count));
-  else {
-    color%=13;
+static void color(uint color)
+{
+  if (color_256)
+    Scolor(hue_to_ansiNum(((color * 360) / color_count) + start_count));
+  else
+  {
+    color %= 13;
     color++;
-    if(color > 7)
-      printf("\033[9%um",color-7);
+    if (color > 7)
+      printf("\033[9%um", color - 7);
     else
-      printf("\033[3%um",color);
+      printf("\033[3%um", color);
   }
 }
 
-static void pchar(const char* str,uint start) {
+static void pchar(const char *str, uint start)
+{
   uint curent_color = start;
-  while (*str) {
-    if(!rotate_color)
-      curent_color = c_rand()%color_count;
+  while (*str)
+  {
+    if (!rotate_color)
+      curent_color = c_rand() % color_count;
     color(curent_color);
-    printf("%c",*str);
+    printf("%c", *str);
     str++;
     curent_color++;
-    if(curent_color >= color_count)
+    if (curent_color >= color_count)
       curent_color = 0;
   }
 }
 
-static void help(int code) {
+static void help(int code)
+{
   puts("+----------------------------------------+");
   puts("| -h - This message.                     |");
   puts("| -C - Only change whole line color.     |");
@@ -183,64 +217,87 @@ static void help(int code) {
   exit(code);
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
   signal(SIGINT, clear);
   signal(SIGTERM, clear);
   signal(SIGABRT, clear);
   FILE *fp = stdin;
   int opt;
-  while ((opt = getopt(argc, argv, "CrRh5a:s:A:")) != -1) {
-    switch (opt) {
-      case 'C': each_char = false; break;
-      case 'r': random_start_color = true; break;
-      case 'R': rotate_color = false,random_start_color = true; break;
-      case '5': color_256 = false; break;
-      case 'h': help(0); break;
-      case 'a': {
-        errno = 0;
-        color_count = strtol(optarg,NULL,10);
-        if(errno){
-          printf("invalid value for -%c",opt);
-          help(EXIT_FAILURE);
-        }
-        break;
-      }
-      case 's': {
-        errno = 0;
-        start_count = strtol(optarg,NULL,10);
-        if(errno){
-          printf("invalid value for -%c",opt);
-          help(EXIT_FAILURE);
-        }
-        break;
-      }
-      case 'A': {
-        errno = 0;
-        shift_c = strtol(optarg,NULL,10);
-        if(errno){
-          printf("invalid value for -%c",opt);
-          help(EXIT_FAILURE);
-        }
-        break;
-      }
-      default:
+  while ((opt = getopt(argc, argv, "CrRh5a:s:A:")) != -1)
+  {
+    switch (opt)
+    {
+    case 'C':
+      each_char = false;
+      break;
+    case 'r':
+      random_start_color = true;
+      break;
+    case 'R':
+      rotate_color = false, random_start_color = true;
+      break;
+    case '5':
+      color_256 = false;
+      break;
+    case 'h':
+      help(0);
+      break;
+    case 'a':
+    {
+      errno = 0;
+      color_count = strtol(optarg, NULL, 10);
+      if (errno)
+      {
+        printf("invalid value for -%c", opt);
         help(EXIT_FAILURE);
+      }
+      break;
+    }
+    case 's':
+    {
+      errno = 0;
+      start_count = strtol(optarg, NULL, 10);
+      if (errno)
+      {
+        printf("invalid value for -%c", opt);
+        help(EXIT_FAILURE);
+      }
+      break;
+    }
+    case 'A':
+    {
+      errno = 0;
+      shift_c = strtol(optarg, NULL, 10);
+      if (errno)
+      {
+        printf("invalid value for -%c", opt);
+        help(EXIT_FAILURE);
+      }
+      break;
+    }
+    default:
+      help(EXIT_FAILURE);
     }
   }
 
-  if(*argv[argc-1] != '-' && argc > 1) {
+  if (*argv[argc - 1] != '-' && argc > 1)
+  {
     char var_opts[] = "asA";
     bool file = true;
-    for(size_t i = 0;i < sizeof(var_opts)-1;i++){
+    for (size_t i = 0; i < sizeof(var_opts) - 1; i++)
+    {
       char opts[] = "-a";
       opts[1] = var_opts[i];
-      if(!strcmp(argv[argc-2],opts))
+      if (!strcmp(argv[argc - 2], opts))
         file = false;
     }
-    if(file) {
-      fp = fopen(argv[argc-1], "r");
-      if (!fp) {
-        fprintf(stderr, "Error opening file '%s'\n", argv[argc-1]);
+    if (file)
+    {
+      fp = fopen(argv[argc - 1], "r");
+      if (!fp)
+      {
+        fprintf(stderr, "Error opening file '%s'\n", argv[argc - 1]);
         return EXIT_FAILURE;
       }
     }
@@ -250,18 +307,19 @@ int main(int argc, char **argv){
   size_t len = 0;
   ssize_t lineSize = 0;
   int curent_color = 0;
-  while ((lineSize = getline(&line, &len, fp)) != -1) {
-    if(random_start_color)
-      curent_color = c_rand()%color_count;
+  while ((lineSize = getline(&line, &len, fp)) != -1)
+  {
+    if (random_start_color)
+      curent_color = c_rand() % color_count;
     color(curent_color);
-    if(each_char)
-      pchar(line,curent_color);
+    if (each_char)
+      pchar(line, curent_color);
     else
-      printf("%s",line);
-    curent_color+= shift_c;
-    if(curent_color <= 0)
-      curent_color = color_count-1;
-    if(curent_color >= color_count)
+      printf("%s", line);
+    curent_color += shift_c;
+    if (curent_color <= 0)
+      curent_color = color_count - 1;
+    if (curent_color >= color_count)
       curent_color = 0;
   }
   free(line);
